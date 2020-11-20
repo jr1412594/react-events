261b356930e49c1bad292bb74e605ac036a914a3
jr1412594 <jr.medina1412@gmail.com>
2020-11-20 14:35:22 -0700

    component files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Component } from 'react'
 
+import ComponentOne from './ComponentOne'
+
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-  
+        <ComponentOne />
       </div>
     );
   }
